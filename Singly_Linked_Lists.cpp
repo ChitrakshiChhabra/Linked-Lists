cdd503a3de53ed4806b9d3bbe460f4592878fe7d
Chitrakshi <chitrakshi.chhabra@gmail.com>
2022-09-01 09:59:46 +0530

    Added recursive display and sum

--- Singly_Linked_Lists.cpp
+++ Singly_Linked_Lists.cpp
@@ -6,23 +6,22 @@ struct Node
     int data;
     Node *next = NULL;
 
-}*first = new Node,
- *last;
+} *first = new Node,
+  *last;
 
 // Function to create the linked list.
 void creation(int values[], int number)
 {
-    first -> data = values[0];
+    first->data = values[0];
     Node *current_node, *previous_node;
     previous_node = first;
 
     for (int i = 1; i < number; i++)
     {
         current_node = new Node;
-        current_node -> data = values[i];
-        previous_node -> next = current_node;
+        current_node->data = values[i];
+        previous_node->next = current_node;
         previous_node = current_node;
-
     }
 
     last = current_node;
@@ -37,8 +36,8 @@ void traversal()
 
     while (true)
     {
-        cout << n -> data;
-        if (n -> next == NULL)
+        cout << n->data;
+        if (n->next == NULL)
         {
             cout << endl;
             break;
@@ -46,8 +45,26 @@ void traversal()
         else
         {
             cout << " -> ";
-            n = n -> next;
+            n = n->next;
         }
+    }
+}
+
+// Fucntion to display a Linked List using Recursion.
+
+void recursive_display(Node *n = first)
+{
+    cout << n->data;
+
+    if (n->next == NULL)
+    {
+        cout << endl;
+        return;
+    }
+    else
+    {
+        cout << " -> ";
+        recursive_display(n->next);
     }
 }
 
@@ -55,11 +72,11 @@ void traversal()
 void insert_in_beginning(int node_data)
 {
     Node *new_first = new Node;
-    new_first -> data = node_data;
+    new_first->data = node_data;
 
     Node *old_first = first;
     first = new_first;
-    first -> next = old_first;
+    first->next = old_first;
 
     cout << "Element successfully inserted in the beginning of the Linked List." << endl;
 }
@@ -68,20 +85,19 @@ void insert_in_beginning(int node_data)
 void insert_in_the_end(int node_data)
 {
     Node *new_end = new Node;
-    new_end -> data = node_data;
+    new_end->data = node_data;
 
     Node *old_end = last;
     last = new_end;
-    old_end -> next = last;
+    old_end->next = last;
 
     cout << "Element successfully inserted at the end of the Linked List." << endl;
-    
 }
 // Function to insert a node/element at a position in a linked list
 void insert_at_position(int position, int node_data)
 {
     Node *new_between = new Node;
-    new_between -> data = node_data;
+    new_between->data = node_data;
 
     Node *pos = first;
     int counter = 1;
@@ -92,26 +108,25 @@ void insert_at_position(int position, int node_data)
             break;
         else
         {
-            pos = pos -> next;
-            counter+=1;
+            pos = pos->next;
+            counter += 1;
         }
     }
 
     Node *later;
-    later = pos -> next;
+    later = pos->next;
 
-    pos -> next = new_between;
-    new_between -> next = later;
+    pos->next = new_between;
+    new_between->next = later;
 
-    cout << "Element successfully inserted at position " << position <<" in the Linked List." << endl;
-
+    cout << "Element successfully inserted at position " << position << " in the Linked List." << endl;
 }
 
 // Function to delete a node in the beginning of the linked list.
 
 void delete_in_beginning()
 {
-    Node *n = first -> next;
+    Node *n = first->next;
     first = n;
 
     cout << "Element successfully deleted in the beginning of the Linked List." << endl;
@@ -120,17 +135,17 @@ void delete_in_beginning()
 void delete_at_the_end()
 {
     Node *p = first;
-    while(true)
+    while (true)
     {
-        Node *r = p -> next;
+        Node *r = p->next;
 
-        if (r -> next == NULL)
+        if (r->next == NULL)
             break;
         else
-            p = p -> next;
+            p = p->next;
     }
 
-    p -> next = NULL;
+    p->next = NULL;
     last = p;
 
     cout << "Element successfully deleted at the end of the Linked List." << endl;
@@ -144,22 +159,20 @@ void delete_at_position(int position)
     int counter = 1;
     before = first;
 
-    while(true)
-    {   
+    while (true)
+    {
         if (counter + 1 == position)
         {
-            Node *n = before -> next;
-            after = n -> next;
-            before -> next = after;
+            Node *n = before->next;
+            after = n->next;
+            before->next = after;
             break;
         }
         else
         {
-            before = before -> next;
-            counter+=1;
-
+            before = before->next;
+            counter += 1;
         }
-        
     }
 
     cout << "Element successfully deleted at position " << position << " of Linked List." << endl;
@@ -172,26 +185,36 @@ void number_of_nodes()
     Node *n = first;
     int count = 0;
 
-    while(true)
+    while (true)
     {
-        count+=1;
-        if(n -> next == NULL)
+        count += 1;
+        if (n->next == NULL)
             break;
         else
-            n = n -> next;
+            n = n->next;
     }
 
     cout << "Number of nodes in the Linked List are " << count << "." << endl;
 }
 
+// Recursive sum of elements in a linked list
 
+void recursive_sum(Node *n = first, int sum = 0)
+{
+    sum += n->data;
 
-
-
+    if (n->next == NULL)
+    {
+        cout << "Sum of all elements is " << sum << "." << endl;
+        return;
+    }
+    else
+        recursive_sum(n->next, sum);
+}
 
 int main()
 {
-    int values[] = {1,2,3,4,5};
+    int values[] = {1, 2, 3, 4, 5};
     int number = 5;
 
     creation(values, number);
@@ -209,7 +232,8 @@ int main()
     delete_at_position(3);
     traversal();
     number_of_nodes();
+    recursive_display();
+    recursive_sum();
 
     return 0;
-    
 }
