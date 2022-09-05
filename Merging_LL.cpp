07dab7942845d13b696b681b0a7ae58c6fb05e15
Chitrakshi <chitrakshi.chhabra@gmail.com>
2022-09-05 20:36:03 +0530

    Merging Linked List Code + Comments added

--- Merging_LL.cpp
+++ Merging_LL.cpp
@@ -1,5 +1,52 @@
 #include <iostream>
 using namespace std;
+// This code is for merging 2 sorted Linked Lists.
+// eg --
+// List 1   10 -> 30 -> 50 -> 70 -> NULL
+// List 2   20 -> 40 -> 60 -> 80 -> NULL
+// Merged result will be  10 -> 20 -> 30 -> 40 -> 50 -> 60 -> 70 -> 80 -> NULL
+// Approach to this would be --
+// There are 2 pointers third and last
+// third will be pointing to the first element of the merged Linked List.
+// last will be pointed at the current last element.
+// eg -
+// first
+//   |
+//   V
+//   2 -> 8 -> 10 -> 15
+//   4 -> 7 -> 12 -> 14
+//   ^
+//   |
+// second
+// Comparing both first and second data, we see first is lesser than second.
+// So, first will become the first node of the merged list, hence third will be moved to first.
+// The current last node of the merged list is first only, so last pointer will also move to first.
+// Hence the scenario is like this -->
+// first, last, third (all are on first)
+//                  |
+//                  V
+//                  2 -> 8 -> 10 -> 15
+//                  4 -> 7 -> 12 -> 14
+//                  ^
+//                  |
+//                second
+// We will now move first to first -> next and set last -> next = NULL
+// first now points to 8.
+// Comparing first and second again i.e. comaparing 4 and 8, we find that this time second(4) is smaller.
+// Then making last -> next = second, last = second, second = second -> next, last -> next = NULL
+// So the scenario will look like --
+//               third  first
+//                  |    |
+//                  V    V
+//                  2    8 -> 10 -> 15
+//                  |
+//                  V
+//          NULL <- 4    7 -> 12 -> 14
+//                  ^    ^
+//                  |    |            
+//                 last second
+// Performung these steps we loop until either first or second becomes null.
+// Later we just concatenate the remaning elements of the bigger list with the merged list.
 struct Node
 {
     int data;
@@ -58,44 +105,44 @@ void traversal(Node *n)
     }
 }
 
-Node *merge()
+Node *merge(Node *p, Node *q)
 {
-    if (first -> data < second -> data)
+    if (p -> data < q -> data)
     {
-        third = last = first;
-        first = first -> next;
-        last -> next = NULL;
+        third = last = p;
+        p = p -> next;
     }
     else
     {
-        third = last = second;
-        second = second -> next;
-        last -> next = NULL;
+        third = last = q;
+        q = q -> next;
     }
 
-    while(first != NULL || second != NULL)
+    last -> next = NULL;
+
+    while(p != NULL && q != NULL)
     {
-        if (first -> data < second -> data)
+        if (p -> data < q -> data)
         {
-            last -> next = first;
-            last = first;
-            first = first -> next;
+            last -> next = p;
+            last = p;
+            p = p -> next;  
         }
         else
         {
-            last -> next = second;
-            last = second;
-            second = second -> next;
+            last -> next = q;
+            last = q;
+            q = q -> next;
         }
 
         last -> next = NULL;
     }
 
-    if (first != NULL)
-        last -> next = first;
+    if (p != NULL)
+        last -> next = p;
 
-    else
-        last -> next = second;
+    if (q != NULL)
+        last -> next = q;
     
     return third;
 }
@@ -109,7 +156,7 @@ int main()
     traversal(first);
     traversal(second);
     cout << "After Merging the two Linked Lists into one --  ";
-    Node*p = merge();
+    Node*p = merge(first, second);
     traversal(p);
 
     return 0;
